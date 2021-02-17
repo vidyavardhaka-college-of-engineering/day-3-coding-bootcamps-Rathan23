483d7954605fdb6e854881edf02c44c451e13517
Rathan23 <20cse0027@vvce.ac.in>
2021-02-17 11:38:59 +0000

    Problem 4

--- Problem_4.c
+++ Problem_4.c
@@ -6,11 +6,18 @@
 int main()
 {
   	int n;
-
+    int a,b;
   	scanf("%d", &n);
 
   	//Write your code here
-  	
+  	for(a=1;a<=n;a++)
+    {
+      for(b=1;b<=a;b++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }
 
   	return 0;
 }
